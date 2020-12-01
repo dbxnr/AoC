9d452ebf15dc32ef976ea4be330d64a84e5dc998
dbxnr <dbxnr@users.noreply.github.com>
2020-12-01 21:25:03 +0000

    Day 1

---
+++ 1_1/1_1.py
@@ -0,0 +1,9 @@
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+s = 0
+while s <= len(data):
+    for n in data[s:]:
+        if int(n) + int(data[s]) == 2020:
+            print(f"Value is {int(n) * int(data[s])}")
+    s += 1
---
+++ 1_1/1_2.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+import math
+
+with open('input.txt') as f:
+    data = list(map(int, f.readlines()))
+
+
+def permute():
+    p = list(permutations(data, 3))
+    for i in p:
+        if sum(list(i)) == 2020:
+            return math.prod(list(i))
