0b5527dc8a9b649c587e34b47784253ed58b3693
dbxnr <dbxnr@users.noreply.github.com>
2020-12-05 08:15:20 +0000

    day 5 pt 2

--- 5/both.py
+++ 5/both.py
@@ -1,13 +1,8 @@
-data = """BFFFBBFRRR
-FFFBBBFRRR
-BBFFBBFRLL"""
-
-# with open("input.txt") as f:
-#    data = f.read()
-#    data = [line.strip() for line in data]
+with open("input.txt") as f:
+    data = f.readlines()
 
 
-class FindSeat():
+class FindSeat:
     def __init__(self, bpass):
         self.r = list(range(0, 128))
         self.c = list(range(0, 8))
@@ -15,7 +10,7 @@ class FindSeat():
         self.col = [c for c in bpass[7:]]
         self.row_num = self.find_seat(self.row)
         self.col_num = self.find_seat(self.col)
-        # self.seat_num = self.row_num * 8 + self.col_num
+        self.seat_num = self.row_num * 8 + self.col_num
 
     def find_seat(self, lst):
         if len(lst) == 7:
@@ -24,19 +19,40 @@ class FindSeat():
             return self.split(self.c, lst)
 
     def split(self, lst, data):
-        lst = self.upper(lst) if data.pop(0) == 'F' else self.lower(lst)
+        lst = (
+            self.upper(lst)
+            if data[0] == "F" or data[0] == "L"
+            else self.lower(lst)
+        )
         if len(lst) > 1:
-            self.split(lst, data)
-        else:
-            print(lst[0])
-            return lst[0]
+            data.pop(0)
+            return self.split(lst, data)
+
+        return lst[0]
 
     def upper(self, lst):
-        return lst[:len(lst)//2]
+        return lst[: len(lst) // 2]
 
     def lower(self, lst):
-        return lst[len(lst)//2:]
+        return lst[len(lst) // 2:]
 
 
-seat = FindSeat('BFFFBBFRRR')
-print(seat.row_num)
+def find_highest_seat():
+    highest = -1
+    for entry in data:
+        seat_num = FindSeat(entry).seat_num
+        if seat_num > highest:
+            highest = seat_num
+    return highest
+
+
+def find_own_seat():
+    # Hacky
+    all_seats = []
+    for entry in data:
+        all_seats.append(FindSeat(entry).seat_num)
+    all_seats = set(sorted(all_seats))
+    possible_seats = set(range(0, 981))
+    for seat in possible_seats:
+        if seat not in all_seats:
+            print(seat)
